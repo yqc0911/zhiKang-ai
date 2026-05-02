41847ec1c3bd44f9f76906147efab390ee2c871a
yqc0911 <yanqic0911@gmail.com>
2026-05-02 22:56:14 +0800

    完成导航栏的开发设计

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
+//入口文件
+
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
